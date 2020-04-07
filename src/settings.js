60eded847d0eaf54d809e23a4c9c186eba96619e
devide <devide@126.com>
2020-04-07 16:01:02 +0800

    黄金游轮协同平台

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '游轮管理分公司协同平台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}

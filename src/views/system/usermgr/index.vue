6836fb46f5c468f069005c91eae3fcbb163cf836
Administrator <devide@126.com>
2020-04-13 21:22:28 +0800

    1

--- src/views/system/usermgr/index.vue
+++ src/views/system/usermgr/index.vue
@@ -2,6 +2,13 @@
   <div>
     <query-bar @query="querydata">
       <template>
+        <el-button
+          size="small"
+          type="success"
+          icon="el-icon-plus"
+          v-has="{fun:'add'}"
+          @click="AddUser()"
+        >新增</el-button>
         <el-button size="small" type="warning">禁用</el-button>
       </template>
     </query-bar>
@@ -41,6 +48,47 @@
       layout="total, sizes, prev, pager, next"
       @size-change="handleSizeChange"
     ></el-pagination>
+
+    <el-dialog :title="formtitle" :visible.sync="dialogVisible">
+      <el-form :model="userform" class size="small" label-position="right">
+        <el-form-item label="用户编码" label-width="70px">
+          <el-input v-model="userform.usercode"></el-input>
+        </el-form-item>
+        <el-form-item label="用户名" label-width="70px">
+          <el-input v-model="userform.name"></el-input>
+        </el-form-item>
+        <el-form-item label="性别" label-width="70px">
+          <el-radio-group v-model="userform.sex">
+            <el-radio :label="1">男</el-radio>
+            <el-radio :label="0">女</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="登录密码" label-width="70px">
+          <el-input type="password" v-model="userform.userpwd"></el-input>
+        </el-form-item>
+        <el-form-item label="身份证号" label-width="70px">
+          <el-input type="text" v-model="userform.idno"></el-input>
+        </el-form-item>
+        <el-form-item label="联系电话" label-width="70px">
+          <el-input type="tel" v-model="userform.tel"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱地址" label-width="70px">
+          <el-input type="email" v-model="userform.email"></el-input>
+        </el-form-item>
+        <el-form-item label="联系地址" label-width="70px">
+          <el-input type="textarea" autosize="{ minRows: 3, maxRows: 5}" v-model="userform.adress"></el-input>
+        </el-form-item>
+        <el-form-item label="用户头像" label-width="70px">
+          <el-upload show-file-list="false" multiple="false">
+            <el-button size="mini" type="primary" icon="el-icon-upload">上传图片</el-button>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -53,7 +101,19 @@ export default {
   },
   data() {
     return {
+      formtitle: "新增用户",
+      dialogVisible: false,
       list: [],
+      userform: {
+        sex: 1,
+        status: 1,
+        usercode: "",
+        name: "",
+        idno: "",
+        tel: "",
+        email: "",
+        adress: ""
+      },
       recordcount: 0,
       pageindex: 1,
       pagesize: 50
@@ -70,6 +130,9 @@ export default {
       UserFun.userlist().then(res => {
         this.list = res.result.data;
       });
+    },
+    AddUser() {
+      this.dialogVisible = true;
     },
     handleSelectionChange() {},
     handleCurrentChange() {},
